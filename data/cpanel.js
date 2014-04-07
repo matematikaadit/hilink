9a7e9543f08d65826efb8b0a6b179fd15d96c753
Adit Cahya Ramadhan <matematika.adit@gmail.com>
2014-04-07 22:13:25 +0700

    Add basic functionality

---
+++ data/cpanel.js
@@ -0,0 +1,10 @@
+var homeLink = document.getElementById("home");
+var connectLink = document.getElementById("connect");
+var disconnectLink = document.getElementById("disconnect");
+var statusContainer = document.getElementById("status");
+
+homeLink.onclick = function(e) {
+    e.preventDefault();
+
+    self.port.emit("openHome", "");
+};
