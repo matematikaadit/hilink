af35f246ec1e334535cabee72cc4b47072b2a652
Adit Cahya Ramadhan <matematika.adit@gmail.com>
2014-04-12 09:26:02 +0700

    move code around

--- data/cpanel.js
+++ data/cpanel.js
@@ -3,11 +3,11 @@ var connectLink = document.getElementById("connect");
 var disconnectLink = document.getElementById("disconnect");
 var statusContainer = document.getElementById("status");
 
+self.port.on("changeStatus", function(status){
+    statusContainer.innerHTML = status;
+});
+
 homeLink.onclick = function(e) {
     e.preventDefault();
     self.port.emit("openHome", null);
 };
-
-self.port.on("changeStatus", function(status){
-    statusContainer.innerHTML = status;
-});
