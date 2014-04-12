ea5fa0e5d4d740955800de286392c4bc640ada61
Adit Cahya Ramadhan <matematika.adit@gmail.com>
2014-04-12 12:49:21 +0700

    Refactoring, using Request and send the response.text for "status"

--- data/cpanel.js
+++ data/cpanel.js
@@ -4,8 +4,32 @@ var disconnectLink = document.getElementById("disconnect");
 var statusContainer = document.getElementById("status");
 
 self.port.on("changeStatus", function(status){
-    statusContainer.innerHTML = status;
+    var parser = new DOMParser();
+    var xml = parser.parseFromString(status, "application/xml");
+    var elements = xml.getElementsByTagName("ConnectionStatus");
+    var statusString = "";
+    if (elements) {
+        statusString = num2Status(elements[0].innerHTML);
+    }
+    statusContainer.innerHTML = statusString;
 });
+
+var num2Status = function(text) {
+    var statusMap = {
+        "900": "connecting",
+        "901": "connected",
+        "902": "disconnected",
+        "903": "disconnecting"
+    };
+    var statusString = null;
+    if (statusMap[text]) {
+        statusString = statusMap[text];
+    }
+    else {
+        statusString = "unknown";
+    }
+    return statusString;
+};
 
 homeLink.onclick = function(e) {
     e.preventDefault();
